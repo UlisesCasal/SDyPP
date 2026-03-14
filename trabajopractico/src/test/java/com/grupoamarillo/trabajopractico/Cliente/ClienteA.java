62b1c733801c73ee492f1514e968d839391f24a4
Ulises Casal <83846014+UlisesCasal@users.noreply.github.com>
2026-03-14 20:20:42 -0300

    Hit #2 Completado

--- trabajopractico/src/test/java/com/grupoamarillo/trabajopractico/Cliente/ClienteA.java
+++ trabajopractico/src/test/java/com/grupoamarillo/trabajopractico/Cliente/ClienteA.java
@@ -1,38 +1,42 @@
 package com.grupoamarillo.trabajopractico.Cliente;
+
 import java.io.*;
 import java.net.*;
 
 public class ClienteA {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String host = "localhost";
         int puerto = 5001;
+        while (true) {
+            try {
+                // Crea un nuevo Socket, especificando q es local y en el puerto
+                Socket socket = new Socket(host, puerto);
 
-        try {
-            //Crea un nuevo Socket, especificando q es local y en el puerto 
-            Socket socket = new Socket(host, puerto);
-            
-            //Crea un PrintWriter para enviar datos al servidor a traves del socket
-            PrintWriter salida = new PrintWriter(
-                    socket.getOutputStream(), true);
+                // Crea un PrintWriter para enviar datos al servidor a traves del socket
+                PrintWriter salida = new PrintWriter(
+                        socket.getOutputStream(), true);
 
-            //Crea un BufferedReader para recibir datos del servidor a traves del socket
-            BufferedReader entrada = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            
-            //Envía un mensaje al servidor
-            salida.println("Hola B, soy A");
+                // Crea un BufferedReader para recibir datos del servidor a traves del socket
+                BufferedReader entrada = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
 
-            //Recibe la respuesta del servidor
-            String respuesta = entrada.readLine();
-            System.out.println("Servidor responde: " + respuesta);
+                // Envía un mensaje al servidor
+                salida.println("Hola B, soy A");
 
-            //Cierra el socket
-            socket.close();
+                // Recibe la respuesta del servidor
+                String respuesta = entrada.readLine();
+                System.out.println("Servidor responde: " + respuesta);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+                // Cierra el socket
+                socket.close();
+                break;
+            } catch (IOException e) {
+                //e.printStackTrace();
+                System.out.println("Error al conectar con el servidor: " + e.getMessage());
+                Thread.sleep(5000); //Espero 5 segundos y vuelvo a correr el loop
+            }
         }
     }
 }
