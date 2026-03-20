a41f88888eff6e4c9f9b43a2f065748ff2042310
CristianAnito <c_015_2010@outlook.com>
2026-03-20 15:36:46 -0300

    Añadido Readme y test

--- trabajopractico/src/main/java/com/grupoamarillo/trabajopractico/ClienteA.java
+++ trabajopractico/src/main/java/com/grupoamarillo/trabajopractico/ClienteA.java
@@ -1,7 +1,10 @@
-package com.grupoamarillo.trabajopractico.Cliente;
+package com.grupoamarillo.trabajopractico;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ClienteA {
 
@@ -33,9 +36,9 @@ public class ClienteA {
                 socket.close();
                 break;
             } catch (IOException e) {
-                //e.printStackTrace();
+                // e.printStackTrace();
                 System.out.println("Error al conectar con el servidor: " + e.getMessage());
-                Thread.sleep(5000); //Espero 5 segundos y vuelvo a correr el loop
+                Thread.sleep(5000); // Espero 5 segundos y vuelvo a correr el loop
             }
         }
     }
